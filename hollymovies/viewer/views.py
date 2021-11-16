76dda95e301c172cb407e17e6f9d6688bc7fa156
Łukasz Okoń <l.okon@haso.pl>
2021-11-16 20:46:59 +0100

    feature: add GenreDetailView

--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -22,6 +22,11 @@ class GenreDetailView(DetailView):
     template_name = 'genre_detail.html'
     context_object_name = 'genre'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['movies'] = Movie.objects.filter(genre=self.get_object())
+        return context
+
 
 class GenreUpdateView(UpdateView):
     template_name = 'forms.html'
