daafae86bca20feac08585861f2d496dea69da7a
lukaszokon <lukasz.okon12@gmail.com>
2021-12-06 18:54:48 +0100

    add password change custom form and view

---
+++ hollymovies/accounts/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import PasswordChangeForm
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- hollymovies/accounts/views.py
+++ hollymovies/accounts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordChangeView
+
+from .forms import CustomPasswordChangeForm
 
 
 class CustomLoginView(LoginView):
     template_name = 'forms.html'
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'password_change.html'
+    success_url = reverse_lazy('movies')
+    form_class = CustomPasswordChangeForm
