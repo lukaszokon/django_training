4da10c1389b3403bcfa607c8017e05d4ba4fe846
lukaszokon <lukasz.okon12@gmail.com>
2021-12-06 20:18:34 +0100

    add profile one-to-one with django user

--- hollymovies/accounts/forms.py
+++ hollymovies/accounts/forms.py
@@ -1,4 +1,8 @@
+from django.db.transaction import atomic
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
+from django.forms import CharField, Textarea
+
+from .models import Profile
 
 
 class CustomPasswordChangeForm(PasswordChangeForm):
@@ -12,11 +16,19 @@ class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'first_name']
 
+    biography = CharField(label='Your story', widget=Textarea, min_length=20)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
 
+    @atomic
     def save(self, commit=True):
         self.instance.is_active = False
-        return super().save(commit)
+        user = super().save(commit)
+        biography = self.cleaned_data['biography']
+        profile = Profile(biography=biography, user=user)
+        if commit:
+            profile.save()
+        return user
