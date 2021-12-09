db3888ea7b7d9fd3c278688126600ed8510f46bc
lukaszokon <lukasz.okon12@gmail.com>
2021-12-09 19:00:12 +0100

    change admin site

--- hollymovies/viewer/admin.py
+++ hollymovies/viewer/admin.py
@@ -1,6 +1,49 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import Genre, Movie
 
-admin.site.register(Genre)
-admin.site.register(Movie)
+
 # Register your models here.
+
+class MovieAdmin(ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'created']}),
+        (
+            'External Information',
+            {
+                'fields': ['genre', 'released'],
+                'description': (
+                    'These fields are going to be filled with data parsed '
+                    'from external databases.'
+                )
+            }
+        ),
+        (
+            'User Information',
+            {
+                'fields': ['rating', 'description'],
+                'description': 'These fields are intended to be filled in by our users.'
+            }
+        )
+    ]
+    readonly_fields = ['created']
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description='')
+
+    ordering = ['id']
+    list_display = ['id', 'title', 'genre', 'released_year']
+    list_display_links = ['id', 'title']
+    list_per_page = 20
+    list_filter = ['genre']
+    search_fields = ['title']
+    actions = ['cleanup_description']
+
+
+admin.site.register(Genre)
+admin.site.register(Movie, MovieAdmin)
